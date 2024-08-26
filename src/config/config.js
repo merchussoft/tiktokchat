760fadec2949a8f7d02be20ae569a600e02c1d07
server_merchus <merchussoft@hotmail.com>
2024-08-25 21:03:05 -0500

    se hace actualizacion de estilos y el deploy a gh-pages

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,9 @@
 import io from 'socket.io-client';
 
 
-export const SOCKET_URL = 'http://192.168.1.10:3000';
+//export const SOCKET_URL = 'http://192.168.1.10:3000';
+
+export const SOCKET_URL = 'https://tiktokchatapi.onrender.com';
+
 
 export const SOCKET = io(SOCKET_URL);
